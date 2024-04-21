74ccf25d9577dc48052504a350c9b6318a4ac870
Ankit Bisht <148742105+Ankitbisht01@users.noreply.github.com>
2024-04-21 16:29:26 +0000

    adding comment

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ for i in range(n):
   print("x "*(i+1), end=" \n")
 
 print()
-
+#inverted pyramid
 for i in range(n):
   print(" "*(n-i-1), " x"*(i+1), end ="\n")
 
@@ -12,4 +12,6 @@ for i in range(1, n+1):
   for j in range(i):
     print(i, end = " ")
   print()
+
+
   
