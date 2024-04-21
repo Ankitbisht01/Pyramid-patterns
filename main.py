98f68241b4d9c3890bf66fa3efbf4af81ce241b9
Ankit Bisht <30162213-ankitbisht0107@users.noreply.replit.com>
2024-04-21 16:25:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+n = int(input("enter number of rows: "))
+print("simple pyramid pattern")
+for i in range(n):
+  print("x "*(i+1), end=" \n")
+
+print()
+
+for i in range(n):
+  print(" "*(n-i-1), " x"*(i+1), end ="\n")
+
+for i in range(1, n+1):
+  for j in range(i):
+    print(i, end = " ")
+  print()
+  
